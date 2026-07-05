3bd6557bf7e85f4a26a94aecea1813208a561b2f
mb7025191-design <mb7025191@gmail.com>
2026-07-05 13:44:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the 'YouTube Channel' Name Generator:\n" )
+nickName = input("What is your name? ")
+about_channel = input (" what is channel about? \n")
+print ("you could name your channel ( " , about_channel , " with " , nickName , " )")
